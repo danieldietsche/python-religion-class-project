7cff2d754b993d3a2d011554c62192d1fe623fa0
danieldietsche <daniel.dietsche@hope.edu>
2016-04-13 14:30:12 -0400

    initial
    initial files

---
+++ angel_check.py
@@ -0,0 +1,29 @@
+import nltk
+import os
+import sys
+
+input_text = sys.argv[1]
+
+from nltk.corpus import PlaintextCorpusReader
+
+corpus_root = "."
+search_text = nltk.Text(PlaintextCorpusReader(corpus_root, input_text).words())
+
+search_text.concordance("angel",150,25)
+
+search_text.concordance("angels",150,25)
+
+search_text.dispersion_plot(["angel","angels"])
+
+from nltk.corpus import stopwords
+stopwords = nltk.corpus.stopwords.words('bible')
+# stopwords in AppData\Roaming\nltk_data\corpora\stopwords
+search_text = [word for word in search_text if word.lower() not in stopwords]
+
+fd = nltk.FreqDist(search_text)
+
+print "Number of times used:"
+for (key, value) in (fd.items()):
+	if key == "angel" or key == "angels":
+		print key,value
+
